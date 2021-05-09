4a87a647ee913e502aa888addc54c7f397d56d5e
waqas <waqas.orthocare@nxvt.com>
2021-05-09 20:44:11 +0500

    updates

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ export const environment = {
   baseUrl : 'https://www.citygrabs.com',
   apiBaseUrl : 'https://www.citygrabs.com/api/public/api',
   imageBaseUrl: 'https://www.citygrabs.com/api/public',
-  STRIPE_KEY: 'pk_test_bEAMH4DR5R1TOtiWVN7YGZbB',
-  STRIPE_SECRET: 'sk_test_orabQwYWWZ1RpZA7VftIGMGq',
+  STRIPE_KEY: 'pk_test_XfqiesMVKkD9E4CLzTdvh31L',
+  STRIPE_SECRET: 'sk_test_LEvFI2SuzT41YFjW6U51NrbT',
   adminUrl: 'https://www.citygrabs.com/api/public'
 };
