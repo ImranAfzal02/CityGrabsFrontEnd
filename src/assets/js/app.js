7d89d104d19a747a237d924e3fddfc02c4c418d6
waqas <waqas.orthocare@nxvt.com>
2021-05-10 20:23:34 +0500

    updates

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,7 +1,29 @@
 $(document).ready(function(){
 
-    // $('.toggle').click(function(){
-    //     $('.toggle').toggleClass('active')
-    // })
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+    })
+
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        lazyLoad:true,
+        dots:false,
+        autoplay:true,
+        autoplayTime:2000,
+        responsive:{
+            0:{
+                items:3,
+            },
+            767:{
+                items:6,
+            },
+            1200:{
+                items:9
+            }
+        }
+    }) 
 
 });
